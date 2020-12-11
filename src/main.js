7b5219319261d10dc8e9dcd989c71485772f79a3
dmmaia <david_mmaia@hotmail.com>
2020-12-11 13:51:34 -0300

    'keypress' package installed

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var keypress = require('keypress');
+var log = []
+ 
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+ 
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  log.push(key.sequence)
+  console.log(log);
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+});
+ 
+process.stdin.setRawMode(true);
+process.stdin.resume();
