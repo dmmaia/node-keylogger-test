30dce223d085994acf8192176079b4f2ee391808
dmmaia <david_mmaia@hotmail.com>
2020-12-11 15:22:44 -0300

    write key log function added

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 var keypress = require('keypress');
-var log = []
+const fs = require('fs');
+
+var log = ''
  
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
  
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
-  log.push(key.sequence)
-  console.log(log);
+  if(key){
+    log = `${log}\n${new Date()}: ${key.sequence}`
+
+    try {
+      fs.writeFileSync("./tmp/log.txt", log);
+    } catch (error) {
+      console.log(error)
+    }
+  }
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
   }
